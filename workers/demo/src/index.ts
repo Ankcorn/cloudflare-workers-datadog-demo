eb0498846a0628a7bae8cb87cb9ff8e2a7108135
Ankcorn <thomasankcorn@gmail.com>
2025-05-13 16:28:58 +0100

    add tracing

--- workers/demo/src/index.ts
+++ workers/demo/src/index.ts
@@ -1,4 +1,5 @@
 
+import { instrument, type ResolveConfigFn } from '@microlabs/otel-cf-workers'
 import api from "./api";
 
 
@@ -8,4 +9,14 @@ const handler =  {
 	}
 };
 
-export default handler;
+const config: ResolveConfigFn = (env: Env) => {
+	return {
+		exporter: {
+			url: 'https://trace.agent.us3.datadoghq.com/api/v0.2/traces',
+			headers: { 'DD-API-KEY': env.DATADOG_API_KEY },
+		},
+		service: { name: 'greetings' },
+	}
+}
+
+export default instrument(handler, config)
